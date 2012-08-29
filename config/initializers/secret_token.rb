ea3144b14916ac8e52bf388b9fc7d5a8c2f53d4f
Kunal Bhatia <kunalb@weboniselab.com>
2012-08-29 11:41:48 +0530

    create a new app winnitude

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winnitude::Application.config.secret_token = '46b3803eadad0d47546e7155a643a755ff2fc20408b89dfcf7987a934b84b6c4b40b8f0027f7ef9b6652bcb99487b739788b3d32ac04dc51cdd7147fa9c9d63d'
