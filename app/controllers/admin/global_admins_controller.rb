d1d1805a608e79383dcc5e2a1774ac56165f4136
Kunal Bhatia <kunalb@weboniselab.com>
2012-08-31 14:57:23 +0530

    make selected language active and applying the filteration for country and currency at PT setup page

--- app/controllers/admin/global_admins_controller.rb
+++ app/controllers/admin/global_admins_controller.rb
@@ -1,13 +1,15 @@
 class Admin::GlobalAdminsController < ApplicationController
   class Admin::GlobalAdminsController < ApplicationController
     def set_platform_page
-      @countries = CountryDetail.all.collect{|i|i.country_english_name}
-      @currency = Currency.all.collect{|i| i.description}
+      countries = CountryDetail.where.all
+      @user_countries = countries.select{|i| i.is_active == 1 && i.user_country ==1 }.collect{|i|i.country_english_name}
+      @master_country = countries.select{|i| i.is_active == 1 && i.is_default ==1 }.collect{|i|i.country_english_name}
+      @currency = Currency.all.select{|i| i.is_active == 1 }.collect{|i| i.description}
       @languages = Language.all.collect{|i| i.english_name}
     end
 
     def set_platform
-
+      Language.make_language_active(params[:language])
       @user= User.create_global_admin_owner(params)
       @general_settings = GeneralSetting.initialize_global_admin_general_settings(@user, params)
       render :json => @general_settings
--- app/models/language.rb
+++ app/models/language.rb
@@ -4,4 +4,9 @@ class Language
   field :local_name, :type => String
   field :english_name, :type => String
   field :iso_code, :type => String
+
+  def self.make_language_active(language)
+    lang = where(:english_name => language).first
+    lang.update_attribute(:is_active, 1)
+  end
 end
