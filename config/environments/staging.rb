e23c415a6b111e902fa075814573cbf2dd0b0b03
Kunal Bhatia <kunalb@weboniselab.com>
2012-08-31 16:39:43 +0530

    creating new branch testing to test

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-CrowdFunding::Application.configure do
+Winnitude::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
