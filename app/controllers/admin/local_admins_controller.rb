d686b37d3432833257fc9b3d8712fdb724894d94
Kunal Bhatia <kunalb@weboniselab.com>
2012-09-20 18:21:28 +0530

    change in gem file for its log

--- app/controllers/admin/local_admins_controller.rb
+++ app/controllers/admin/local_admins_controller.rb
@@ -1,6 +1,7 @@
 class Admin::LocalAdminsController < ApplicationController
   before_filter :should_be_global_admin
  def index
-   @local_admins = PlatformLocalAdmin.all.includes[:la_general_setting]
+   @local_admins = PlatformLocalAdmin.all.includes([:la_general_setting,:user])
+   @local_admins = @local_admins.paginate(:page => params[:page], :per_page => 10)
  end
 end
