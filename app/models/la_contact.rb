2f1fb282d296aae37e723a7f996608b9aa4c3efd
Kunal Bhatia <kunalb@weboniselab.com>
2012-10-01 17:16:47 +0530

    add carrierwave gem

--- app/models/la_contact.rb
+++ app/models/la_contact.rb
@@ -5,5 +5,5 @@ class LaContact
   field :contact_last_name,                                    :type => String
   field :contact_email,                                        :type => String
   field :contact_biography,                                    :type => String
-  #mount_uploader :contact_photo, ImageUploader
+  mount_uploader :contact_photo, ImageUploader
 end
