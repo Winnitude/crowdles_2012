86a296cf942e67d6ff9e5c1fccb5734d3e533654
Kunal Bhatia <kunalb@weboniselab.com>
2012-10-23 11:23:04 +0530

    fix the bug at date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require bootstrap-datepicker
 //= require autocomplete-rails
-//= require jquery-ui
 //= require_tree .
 
 
